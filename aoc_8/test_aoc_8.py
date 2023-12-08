890e1f918278c9435c2ed20261cc9f3f8a778f79
Karsten Lettow <karsten.lettow@thoughtworks.com>
2023-12-08 16:34:21 +0100

    Chores

--- aoc_8/test_aoc_8.py
+++ aoc_8/test_aoc_8.py
@@ -1,4 +1,10 @@
-from aoc_8.aoc_8 import follow_complex_instructions, follow_instructions, least_common_multiple, set_complex_dict, set_dict
+from aoc_8.aoc_8 import (
+  follow_complex_instructions,
+  follow_instructions,
+  least_common_multiple,
+  set_complex_dict,
+  set_dict,
+)
 
 
 class TestSetDict:
@@ -9,17 +15,22 @@ class TestSetDict:
     assert set_dict(["FLG = (PCR, CTD)"]) == {"FLG": ("PCR", "CTD")}
 
   def test_multi_line(self):
-    assert set_dict(["AAA = (BBB, CCC)", "DDD = (EEE, FFF)"]) == {"AAA": ("BBB", "CCC"), "DDD": ("EEE", "FFF")}
+    assert set_dict(["AAA = (BBB, CCC)", "DDD = (EEE, FFF)"]) == {
+      "AAA": ("BBB", "CCC"),"DDD": ("EEE", "FFF")}
 
 class TestFollowInstructions:
   def test_single_instruction(self):
     assert follow_instructions("R", {"AAA": ("BBB", "ZZZ")}) == 1
 
   def test_two_step_instructions(self):
-    assert follow_instructions("LR", {"AAA": ("BBB", "CCC"), "BBB": ("DDD", "ZZZ")}) == 2
+    assert follow_instructions("LR", {
+      "AAA": ("BBB", "CCC"),
+      "BBB": ("DDD", "ZZZ")}) == 2
 
   def test_two_step_single_instruction(self):
-    assert follow_instructions("L", {"AAA": ("BBB", "CCC"), "BBB": ("ZZZ", "DDD")}) == 2
+    assert follow_instructions("L", {
+      "AAA": ("BBB", "CCC"),
+      "BBB": ("ZZZ", "DDD")}) == 2
 
   def test_single_z_version(self):
     assert follow_instructions("R", {"AAA": ("BBB", "XXZ")}) == 1
@@ -30,17 +41,25 @@ class TestFollowInstructions:
 
 class TestComplexDict:
   def test_single_ending_on_a(self):
-    assert set_complex_dict(["AAA = (BBB, CCC)"]) == ({"AAA": ("BBB", "CCC")}, {"A": ["AAA"]})
+    assert set_complex_dict(["AAA = (BBB, CCC)"]) == (
+      {"AAA": ("BBB", "CCC")},
+      {"A": ["AAA"]})
 
   def test_two_ending_on_a(self):
-    assert set_complex_dict(["AAA = (BBB, CCC)", "DDA = (EEE, FFF)"]) == ({"AAA": ("BBB", "CCC"), "DDA": ("EEE", "FFF")}, {"A": ["AAA", "DDA"]})
+    assert set_complex_dict(["AAA = (BBB, CCC)", "DDA = (EEE, FFF)"]) == ({
+      "AAA": ("BBB", "CCC"),
+      "DDA": ("EEE", "FFF")},
+      {"A": ["AAA", "DDA"]})
 
 class TestComplexFollowInstructions:
   def test_single_step(self):
-    assert follow_complex_instructions("R", {"AAA": ("BBB", "ZZZ")}, {"A": ["AAA"]}) == 1
+    assert follow_complex_instructions("R",{
+      "AAA": ("BBB", "ZZZ")}, {"A": ["AAA"]}) == 1
 
   def test_single_step_complex(self):
-    assert follow_complex_instructions("L", {"AAA": ("ZZZ", "BBB"), "BBA": ("BBZ", "CCC")}, {"A": ["AAA", "BBA"]}) == 1
+    assert follow_complex_instructions("L", {
+      "AAA": ("ZZZ", "BBB"),
+      "BBA": ("BBZ", "CCC")}, {"A": ["AAA", "BBA"]}) == 1
 
   def test_two_step_complex(self):
     assert follow_complex_instructions("LR", {
