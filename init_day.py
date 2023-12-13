a9566d7f4bca90a1053a9da9d981551616537ed9
Karsten Lettow <karsten.lettow@thoughtworks.com>
2023-12-13 14:17:53 +0100

    Update on init_day

--- init_day.py
+++ init_day.py
@@ -1,7 +1,7 @@
 import os
-import requests
 from pathlib import Path
 
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,6 +9,7 @@ load_dotenv()
 
 FOLDER = os.path.dirname(__file__)
 AOC_URL = "https://adventofcode.com/2023/day/"
+
 
 def init_day():
   input_day = int(input("Which Advent of Code day do you want to initialize? (1-25)"))
@@ -24,15 +25,17 @@ def init_day():
   Path(folder, "__init__.py").touch()
 
   url = f"{AOC_URL}{input_day}/input"
-  aoc_response = requests.get(url, cookies={"session": os.getenv("SESSION_COOKIE")}, stream=True)
-  with open(os.path.join(folder, "input.txt"), 'wb') as f:
+  aoc_response = requests.get(
+    url, cookies={"session": os.getenv("SESSION_COOKIE")}, stream=True
+  )
+  with open(os.path.join(folder, "input.txt"), "wb") as f:
     for chunk in aoc_response:
       f.write(chunk)
+
 
 def main():
   init_day()
 
+
 if __name__ == "__main__":
   main()
-
-
